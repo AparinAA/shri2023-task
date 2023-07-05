aabe7c8b7855a27e56b88f7f3c45b4f14f4f2c30
AparinAA <alxaparin@gmail.com>
2023-07-05 09:57:46 +0300

    split to bundle

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-setTimeout(function(){const a=ReactDOM.createRoot(document.getElementById("app"));a.render(/*#__PURE__*/React.createElement(React.Fragment,null,/*#__PURE__*/React.createElement(Header,null),/*#__PURE__*/React.createElement(Main,null)))},100);
+const root=ReactDOM.createRoot(document.getElementById("app"));root.render(/*#__PURE__*/React.createElement(React.Fragment,null,/*#__PURE__*/React.createElement(Header,null),/*#__PURE__*/React.createElement(Main,null)));
 
