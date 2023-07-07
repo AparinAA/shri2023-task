9a950b3f97f3b62fca96944aea02360ace2e63a2
AparinAA <alxaparin@gmail.com>
2023-07-07 19:33:49 +0300

    test build

--- index.js
+++
@@ -1,2 +0,0 @@
-const root=ReactDOM.createRoot(document.getElementById("app"));root.render(/*#__PURE__*/React.createElement(React.Fragment,null,/*#__PURE__*/React.createElement(Header,null),/*#__PURE__*/React.createElement(Main,null)));
-
